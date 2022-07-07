8563f93d6b3eeb208d77c3a1eb8d4b8115a18d2a
antonyTheRiskTaker <mrmikadox@gmail.com>
2022-07-07 16:33:04 +0800

    first commit

---
+++ index.js
@@ -0,0 +1,72 @@
+// Require all of the libraries needed
+
+// In-built Node Modules
+const fs = require("fs");
+const path = require("path");
+
+// NPM installed modules
+const basicAuth = require("express-basic-auth");
+const { create } = require("express-handlebars");
+
+const express = require("express");
+const app = express();
+
+// Get all user generated modules into the application
+
+const config = require("./stores/config.json")["development"]; // We use all the development paths
+const AuthChallenger = require("./AuthChallenger");
+const NoteService = require("./Service/NoteService");
+const NoteRouter = require("./Router/NoteRouter");
+
+//The code below returns the current year
+const hbs = create({
+  helpers: {
+    year() {
+      return new Date().getFullYear();
+    },
+  },
+});
+
+// Set up handlebars as our view engine - handlebars will responsible for rendering our HTML
+app.engine("handlebars", hbs.engine);
+app.set("view engine", "handlebars");
+
+// Serves the public directory to the root of our server
+app.use(express.static("public"));
+
+// Set up middleware
+app.use(express.urlencoded({ extended: false }));
+app.use(express.json());
+
+// Set up basic auth
+app.use(
+  basicAuth({
+    authorizer: AuthChallenger(
+      JSON.parse(fs.readFileSync(path.join(__dirname, config.users)))
+    ), // we are defining the file where our users exist with this code: JSON.parse(fs.readFileSync(path.join(__dirname, config.users))), we also parse the data so that we can iterate over each user like a JavaScript variable/ object.
+    challenge: true,
+    realm: "Note Taking Application",
+  })
+);
+
+// Create a new instance of noteService and pass the file path/to/the/file where you want the service to read from and write to.
+const noteService = new NoteService(path.join(__dirname, config.notes), fs);
+
+// Responsible for sending our index page back to our user.
+app.get("/", (req, res) => {
+  console.log(req.auth.user, req.auth.password);
+  noteService.list(req.auth.user).then((data) => {
+    res.render("index", {
+      user: req.auth.user,
+      notes: data,
+    });
+  });
+});
+
+// Set up the NoteRouter - handle the requests and responses in the note, read from a file and return the actual data, get the note from your JSON file and return to the clients browser.
+app.use("/api/notes", new NoteRouter(noteService, express).router()); //sending our data
+
+// Set up the port that we are going to run the application on, therefore the port that we can view the application from our browser.
+app.listen(config.port, () =>
+  console.log(`Note Taking application listening to port ${config.port}`)
+);
---
+++ tests/index.test.js
@@ -0,0 +1,114 @@
+const request = require('supertest');
+const app = require('../index.js');
+
+const tester = request(app)
+
+describe('Routes', () => {
+
+    afterAll(async () => {
+        await new Promise(resolve => setTimeout(() => resolve(), 1000)); // avoid jest open handle error
+    })
+
+    test('GET / it should return 401 if you do not supply a username and password', (done)=>{
+        request(app)
+            .get('/')
+            .expect(401)
+            .end((err, res)=>{
+                if(err) throw err;
+                done()
+            })
+    })
+    test('GET /api/notes it should return 401 if you do not supply a username and password', (done)=>{
+        request(app)
+            .get('/api/notes')
+            .expect(401)
+            .end((err, res)=>{
+                if(err) throw err;
+                done()
+            })
+    })
+    test('POST /api/notes it should return 401 if you do not supply a username and password', (done)=>{
+        request(app)
+            .post('/api/notes')
+            .expect(401)
+            .end((err, res)=>{
+                if(err) throw err;
+                done()
+            })
+    })
+    test('PUT /api/notes/1 it should return 401 if you do not supply a username and password', (done)=>{
+        request(app)
+            .put('/api/notes/1')
+            .expect(401)
+            .end((err, res)=>{
+                if(err) throw err;
+                done()
+            })
+    })
+    test('DELETE /api/notes/1 it should return 401 if you do not supply a username and password', (done)=>{
+        request(app)
+            .delete('/api/notes/1')
+            .expect(401)
+            .end((err, res)=>{
+                if(err) throw err;
+                done()
+            })
+    })
+
+    test('/random it should return 404 if try to go down a route that is not correct', (done)=>{
+
+        let username = 'sam';
+        let password = '1234512345';
+        var auth = 'Basic ' + new Buffer(username + ':' + password).toString('base64');
+    
+
+        request(app)
+            .get('/random')
+            .set('Authorization', auth)
+            .expect(404)    
+            .end((err, res)=>{
+                if(err) throw err;
+                done()
+            })
+    })
+
+    test('/ should return the index page',  (done)=>{
+        let username = 'sam';
+        let password = '1234512345';
+        var auth = 'Basic ' + new Buffer(username + ':' + password).toString('base64');
+
+       var auth =  'Basic c2FtOjEyMzQ1MTIzNDU='
+    
+        request(app)
+       .get('/')
+       
+            .set("Authorization", auth)
+            .expect("Content-Type", "text/html; charset=utf-8")
+            .expect(200)
+            .end(()=>{
+                done()
+            })
+
+           
+            
+    })
+
+    
+
+});
+
+
+/*
+https://www.npmjs.com/package/supertest
+
+https://medium.com/@juha.a.hytonen/testing-authenticated-requests-with-supertest-325ccf47c2bb
+
+https://stackoverflow.com/questions/42490579/supertest-not-working-with-routes-that-call-render
+
+https://github.com/visionmedia/supertest/issues/520
+
+https://stackoverflow.com/questions/52840873/getting-500-internal-server-error-while-using-supertest
+
+
+http://visionmedia.github.io/superagent/#authentication
+*/
